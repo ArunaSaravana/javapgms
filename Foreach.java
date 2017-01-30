a5d75c9e03f041ccd13123b50726857a4faa8b94
ArunaSaravana <arunamuthumari@gmail.com>
2017-01-30 18:17:01 +0530

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class Foreach {
+   
+    public void feach()
+    {
+        int n[]={5,10,15,20,30};
+        
+        for (int j : n) 
+        {
+            System.out.println(j);
+        }
+                
+    }
+}
